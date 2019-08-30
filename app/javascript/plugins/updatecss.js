ce223059cae343494c66db5e9084ce8fe8c4257d
rg <gonera.raphael@gmail.com>
2019-08-30 14:11:03 +0200

    game fini

--- app/javascript/plugins/updatecss.js
+++ app/javascript/plugins/updatecss.js
@@ -1,57 +1,87 @@
+// Enregistrement constances
 const hero = document.getElementById("style-variable1");
 const variable1 = document.getElementById("variable1");
 const variable2 = document.getElementById("variable2");
 const variable3 = document.getElementById("variable3");
+const victory = document.getElementById("victory");
+const deaddragon = document.getElementById("deaddragon");
+const name = document.getElementById("gamename");
+console.log(name.innerHTML);
 
+// Affectation de valeur par défaut
+if (variable1) {variable1.value = "1"}
+if (variable2) {variable2.value = "blue"}
+if (variable3) {variable3.value = "30"}
 
-if (variable1) {
-  variable1.value = "1"
-}
-if (variable2) {
-  variable2.value = "blue"
-}
-if (variable3) {
-  variable3.value = "30"
-}
-
-if (variable1) {
-document.getElementById("variable1").addEventListener('change', (event) => {
-  if (variable1.value === "1"){
-    hero.style.right = `70%`}
-  else if (variable1.value === "2"){
-    hero.style.right = `55%`}
-  else { hero.style.right = `30%`}
-  // hero.style.right = `${variable1.value}%`;
-  });
+// Jeu 1
+if (name.innerHTML != " Trump {") {
+  document.getElementById("variable1").addEventListener('change', (event) =>
+    {
+      if (variable1.value  === "1")
+        { hero.style.right = `70%`}
+      else if (variable1.value === "2")
+        { hero.style.right = `55%`}
+      else
+        {
+          hero.style.right = `30%`
+          victory.style.display = "block"
+          deaddragon.style.display = "block"
+        }
+    });
 };
 
-if (variable2) {
-  hero.style.filter = `saturate(10000%) hue-rotate(225deg)`;
-document.getElementById("variable2").addEventListener('change', (event) => {
-  if (variable2.value === "red"){
-    hero.style.filter = `saturate(1)`}
-  else if (variable2.value === "blue"){
-    hero.style.filter = `saturate(10000%) hue-rotate(225deg)`}
-  else { hero.style.filter = `saturate(10000%) hue-rotate(472deg)`}
-  });
-document.getElementById("variable1").addEventListener('change', (event) => {
-  if (variable1.value === "1"){
-    hero.style.right = `30%`}
-  else if (variable1.value === "2"){
-    hero.style.right = `51%`}
-  else { hero.style.right = `74%`}
-  // hero.style.right = `${variable1.value}%`;
-  });
-};
+// jeu 2 : mouvement de CSS
+if (variable2)
+  {
+    hero.style.filter = `saturate(10000%) hue-rotate(225deg)`;
+    document.getElementById("variable2").addEventListener('change', (event) =>
+      {
+        if (variable2.value === "red")
+          {hero.style.filter = `saturate(1)`}
+        else if (variable2.value === "blue")
+          {hero.style.filter = `saturate(10000%) hue-rotate(225deg)`}
+        else { hero.style.filter = `saturate(10000%) hue-rotate(472deg)`}
+      });
 
-if (variable3) {
-  hero.style.width = `30%`
-document.getElementById("variable3").addEventListener('change', (event) => {
-  if (variable3.value === "10"){
-    hero.style.width = `10%`}
-  else if (variable3.value === "20"){
-    hero.style.width = `20%`}
-  else { hero.style.width = `30%`}
+    document.getElementById("variable1").addEventListener('change', (event) =>
+      {
+        if (variable1.value === "1")
+          {hero.style.right = `30%`}
+        else if (variable1.value === "2")
+          {hero.style.right = `51%`}
+        else { hero.style.right = `74%`}
+      });
+  };
 
-});
-}
+if (variable3)
+  {
+    hero.style.width = `30%`
+    document.getElementById("variable3").addEventListener('change', (event) =>
+      {
+        if (variable3.value === "10")
+          {hero.style.width = `10%`}
+        else if (variable3.value === "20")
+          {hero.style.width = `20%`}
+        else { hero.style.width = `30%`}
+
+      });
+  }
+// jeu 2 affichage du victory
+  if (name.innerHTML === " Trump {")
+    {
+      document.getElementById("variable1").addEventListener(`change`, (event) =>
+        {
+          if (variable3.value === "10" && variable2.value=== "red" && variable1.value==="3")
+            {victory.style.display = "block"}
+        })
+      document.getElementById("variable2").addEventListener(`change`, (event) =>
+        {
+          if (variable3.value === "10" && variable2.value=== "red" && variable1.value==="3")
+            {victory.style.display = "block"}
+        })
+      document.getElementById("variable3").addEventListener(`change`, (event) =>
+        {
+          if (variable3.value === "10" && variable2.value=== "red" && variable1.value==="3")
+            {victory.style.display = "block"}
+        })
+    }
