3df65202174dc440a314d5a03269aa5933ae2fc1
PA Bauduin <bauduinpa@gmail.com>
2019-09-02 17:45:51 +0200

    schema

--- db/migrate/20190829090624_add_two_collomn.rb
+++ db/migrate/20190829090624_add_two_collomn.rb
@@ -2,6 +2,5 @@ class AddTwoCollomn < ActiveRecord::Migration[5.2]
   def change
     add_column :games, :logo, :string
     add_column :games, :instructions, :string
-    add_column :games, :difficulty, :integer
   end
 end
