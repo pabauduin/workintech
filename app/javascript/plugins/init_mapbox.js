8dff976d79202c460d294098228d52b6d620ea23
sara louzai <sara.louzay@gmail.com>
2019-09-04 18:49:38 +0200

    pas de refresh page tentative

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,30 @@
 import mapboxgl from 'mapbox-gl';
 
+const form = document.getElementById("form-show")
+console.log(form)
+form.addEventListener('submit', (event) => {
+  console.dir('inside');
+  event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  const city = document.getElementById('query').value;
+  console.log(city);
+  const base_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
+  fetch(`${base_url}${city}.json?access_token=${mapElement.dataset.mapboxApiKey}`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+    const coords = data.features[0].geometry.coordinates
+    const markers = [{lat: coords[1], lng: coords[0], infoWindow: `<div class="school"><p>Le wagon Bordeaux</p>↵<a href="https://www.google.fr/search?source=hp&amp;ei=nMxvXe_ELPiUk74PgaK_4Aw&amp;q=Le wagon Bordeaux">Site web</a></div>`}]
+    console.log(markers)
+    new mapboxgl.Marker()
+    .setLngLat([ markers[0].lng, markers[0].lat ])
+    .addTo(map);
+    //addMarkersToMap(map, markers);
+    // fitMapToMarkers(map, markers);
+  });
+});
+
+
+
 const mapElement = document.getElementById('map');
 
 const buildMap = () => {
@@ -13,7 +38,7 @@ const buildMap = () => {
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-
+    console.dir(popup)
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup)
@@ -31,6 +56,7 @@ const initMapbox = () => {
   if (mapElement) {
     const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
+    console.log(markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
   }
