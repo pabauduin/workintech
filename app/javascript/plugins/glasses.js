7ef54315041e3edc3f7589855af48f81365d2e20
rg <gonera.raphael@gmail.com>
2019-09-05 10:52:37 +0200

    TOUDOUMM

--- app/javascript/plugins/glasses.js
+++ app/javascript/plugins/glasses.js
@@ -1,5 +1,6 @@
 // getElementbyId().
 const glasses = document.querySelector(".glasses")
+const netflix = document.querySelector(".netflix")
 
 
 if (glasses) {
@@ -9,5 +10,13 @@ setTimeout(function() {
   audioPlay.play();
   glasses.classList.add("down-glasses");
 }, 0);
+}
+
+if (netflix) {
+  const audio = document.querySelector(".audio-play")
+  const audioPlay = new Audio(`${audio.src}`);
+setTimeout(function() {
+  audioPlay.play()
+},   0);
 }
 
